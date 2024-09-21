3657948608a9d92ade94d92d97080e481474af95
hamidioke84 <hamidioke84@gmail.com>
2024-09-22 04:16:01 +0700

    update

---
+++ app/Http/Request.php
@@ -0,0 +1,21 @@
+<?php
+
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ProductController;
+
+// $request = $request->method();
+ 
+// if ($request->isMethod('POST')) {
+//     Route::apiResource('/products', [ProductController::class, 'store']);
+// }elseif ($request->isMethod('GET')) {
+//     if (isset($id)) {
+//         Route::apiResource('/products', [ProductController::class, 'show']);
+//     } else {
+//         Route::apiResource('/products', [ProductController::class, 'index']);
+//     }
+// // Route::apiResource('products', ProductController::class);
+// }elseif ($request->isMethod('PUT')) {
+//     Route::apiResource('/products', [ProductController::class, 'update']);
+// }elseif ($request->isMethod('DELETE')) {
+//     Route::apiResource('/products', [ProductController::class, 'destroy']);
+// }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductController; 
+use Illuminate\Auth\Middleware\Authenticate;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::apiResource('/api/products', ProductController::class);
+Route::apiResource('/products', App\Http\Controllers\ProductController::class);
