5e83217c157974fd9dce8f6137188ebe4365d517
hamidioke84 <hamidioke84@gmail.com>
2024-09-21 22:24:06 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    }
+
+    public function show($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return response()->json($product, 200);
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return response()->json(null, 204);
+    }
+}
